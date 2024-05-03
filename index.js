f7833bc67e1a7de9e6f94e30befbdca9d464e424
Riyan-Mo <Riyan8308151899@gmail.com>
2024-05-03 10:10:30 +0530

    Changed index.js from node to express

--- index.js
+++ index.js
@@ -1,45 +1,49 @@
-const http = require('http');
-const fs = require('fs/promises')
+const express = require("express");
+const fs = require("fs/promises");
+const app = express();
+const port = 8080
 
-http.createServer((req,res)=>{
-    const {url} = req;
+async function readFile(fileName) {
+  try {
+    const content = await fs.readFile(`./${fileName}`, { encoding: "utf8" });
+    return content;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-    if(url=="/"){
-        res.writeHead(200, {"Content-Type": "text/html"})
-        readFile('index.html').then(content=>{
-            res.write(content)
-            res.end()
-        })
-    }
-    else if(url=="/about"){
-        res.writeHead(200, {"Content-Type": "text/html"})
-        readFile('about.html').then(content=>{
-            res.write(content)
-            res.end()
-        })
-    }
-    else if(url=='/contact-me'){
-        res.writeHead(200, {"Content-Type": "text/html"})
-        readFile('contact-me.html').then(content=>{
-            res.write(content)
-            res.end()
-        })
-    }
-    else{
-        res.writeHead(404, {"Content-Type": "text/html"})
-        readFile('404.html').then(content=>{
-            res.write(content)
-            res.end()
-        })
-    }
-}).listen(8080);
+app.get("/", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  readFile("index.html").then((content) => {
+    res.write(content);
+    res.end();
+  });
+});
 
-async function readFile(fileName){
-    try{
-        const content = await fs.readFile(`./${fileName}`, {encoding: 'utf8'});
-        return content;
-    }
-     catch(error){
-        console.log(error);
-     }
-} 
+app.get("/about", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  readFile("about.html").then((content) => {
+    res.write(content);
+    res.end();
+  });
+});
+
+app.get("/contact-me", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  readFile("contact-me.html").then((content) => {
+    res.write(content);
+    res.end();
+  });
+});
+
+app.all("*", (req, res)=>{
+    res.writeHead(404, { "Content-Type": "text/html" });
+    readFile("404.html").then((content) => {
+      res.write(content);
+      res.end();
+    });
+})
+
+app.listen(port, ()=>{console.log(`Example app running on port: ${port}`)});
+
+
